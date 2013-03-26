8dad2b32ff5e0aac843553a1b3bdbd7e9128b62f
ingo <ingo.walz@googlemail.com>
2013-03-26 20:26:27 +0100

    Some dispatch tests

--- tests/AssetManagerTest/DispatchTest.php
+++ tests/AssetManagerTest/DispatchTest.php
@@ -13,13 +13,26 @@ class DispatchTest extends AbstractControllerTestCase
         $this->setApplicationConfig(
             include __DIR__ . '/../_files/application.config.php'
         );
-
         parent::setUp();
     }
 
     public function testStatusCode304OnIfModifiedSinceRequest()
     {
+        $request = $this->getApplication()->getRequest();
+        $request->getHeaders()->addHeaderLine('If-Modified-Since', date("D,d M Y H:i:s T", time()));
+        $this->dispatch('/foo.js');
+        $this->assertResponseStatusCode(304);
+        $content = $this->getApplication()->getResponse()->getContent();
+        $this->assertEquals('', $content);
+    }
+
+    public function testStatusCode200OnIfModifiedSinceRequest()
+    {
+        $request = $this->getApplication()->getRequest();
+        $request->getHeaders()->addHeaderLine('If-Modified-Since', 'Sat, 19 Dec 2000 16:25:03 GMT');
         $this->dispatch('/foo.js');
         $this->assertResponseStatusCode(200);
+        $content = $this->getApplication()->getResponse()->getContent();
+        $this->assertEquals("alert('JS File');" . PHP_EOL, $content);
     }
 }
