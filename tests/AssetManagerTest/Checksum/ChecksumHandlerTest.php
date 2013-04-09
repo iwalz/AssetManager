28cd08010a38920fcdb9a9edfa793c2fcfbcc996
ingo <ingo.walz@googlemail.com>
2013-04-09 21:24:34 +0200

    Checksum tests

--- tests/AssetManagerTest/Checksum/ChecksumHandlerTest.php
+++ tests/AssetManagerTest/Checksum/ChecksumHandlerTest.php
@@ -4,6 +4,7 @@ namespace AssetManagerTest\Checksum;
 
 use AssetManager\Checksum\ChecksumHandler;
 use AssetManager\Checksum\ChecksumHandlerServiceFactory;
+use Assetic\Asset\FileAsset;
 use PHPUnit_Framework_TestCase;
 use Zend\ServiceManager\ServiceManager;
 
@@ -21,5 +22,34 @@ class ChecksumHandlerTest extends PHPUnit_Framework_TestCase
         $checksumHandler->setAsset($asset);
 
         $this->assertEquals('3-4d9c619d53dc0', $checksumHandler->getChecksum());
+    }
+
+    public function testFilterExecution()
+    {
+        $checksumHandler = new ChecksumHandler('etag');
+        $filterManager = $this->getMock('AssetManager\Service\AssetFilterManager', array('setFilters'));
+        $asset = $this->getMock('Assetic\Asset\StringAsset', array(), array('foo'));
+
+        $filterManager->expects($this->once())->method('setFilters');
+
+        $checksumHandler->setAsset($asset);
+        $checksumHandler->setPath('/foo.css');
+        $checksumHandler->setAssetFilterManager($filterManager);
+
+        $checksumHandler->getChecksum();
+
+        $this->assertEquals('/foo.css', $checksumHandler->getPath());
+        $this->assertEquals($asset, $checksumHandler->getAsset());
+        $this->assertEquals($filterManager, $checksumHandler->getAssetFilterManager());
+    }
+
+    /**
+     * @expectedException AssetManager\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Only string or StrategyInterface implementation allowed
+     */
+    public function testWrongStrategy()
+    {
+        $checksumHandler = new ChecksumHandler();
+        $checksumHandler->setStrategy(new \stdClass());
     }
 }
